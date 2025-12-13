08611b799537e31d9720d305ec798c0b7934cfa4
Nikita Tsybliyenko <nikita.dzer.g@gmail.com>
2025-12-13 23:18:49 +0300

    Revert "tmp"
    This reverts commit 98e4053832a31a1434cec7b062019357bee2f4f7.

--- problems/2026/05_syst_array/addr_gen/axi_addr_gen.sv
+++ problems/2026/05_syst_array/addr_gen/axi_addr_gen.sv
@@ -3,10 +3,11 @@ module axi_addr_gen_fixed #(
     parameter SIZE = 4,
     parameter AXI_ADDR_WIDTH = 32
 )(
+    // Clock and reset
     input  logic                      clk,
     input  logic                      rst_n,
     
-    // Start signal
+    // Control signals
     input  logic                      i_start,
     
     // Base addresses
@@ -34,17 +35,15 @@ module axi_addr_gen_fixed #(
 localparam AXI_DATA_WIDTH = WIDTH * SIZE;
 localparam BURST_LEN = SIZE;
 
+// FSM states
 typedef enum logic [1:0] {
     WAIT_B,
-    LOAD_B,
-    WAIT_A
+    WAIT_A,
+    SAVE_C
 } state_t;
     
 state_t state;
-
-logic [AXI_ADDR_WIDTH-1:0] b_addr;
-logic [$clog2(AXI_DATA_WIDTH):0] b_rows_count;
-
+    
 assign arsize = $clog2(AXI_DATA_WIDTH/8)[2:0];
 assign awsize = $clog2(AXI_DATA_WIDTH/8)[2:0];
 
@@ -53,49 +52,38 @@ assign arburst = 2'b01;
 assign awburst = 2'b01;
 
 // AXI4 uses len-1
-assign arlen = state == WAIT_A ? BURST_LEN - 1 : 0;
+assign arlen = BURST_LEN - 1;
 assign awlen = BURST_LEN - 1;
 
-// Matrix B is loaded starting from last  row
-// Matrix A is loaded starting from first row (burst load)
-assign araddr  = state == WAIT_B ? base_addr_b + (SIZE - 1) * AXI_DATA_WIDTH/8 
-               : state == LOAD_B ? b_addr
-                                 : base_addr_a;
-assign arvalid = i_start;
+assign arvalid = i_start & (state == WAIT_B || state == WAIT_A);
+assign araddr  = state == WAIT_B
+    ? base_addr_b : state == WAIT_A
+    ? base_addr_a
+    : 0;
 
-// Matrix C address is stored during matrix
 assign awvalid = i_start & state == WAIT_A;
 assign awaddr = state == WAIT_A ? base_addr_c : 0;
 
+// FSM
 always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         state <= WAIT_B;
-        b_rows_count <= 0;
     end else begin
         case (state)
             WAIT_B: begin
                 if (arvalid & arready) begin
-                    b_addr <= base_addr_b + (SIZE - 2) * AXI_DATA_WIDTH/8;
-                    b_rows_count <= 1;
-                    state <= LOAD_B;
-                end
-            end
-
-            LOAD_B: begin
-                if (arvalid & arready) begin
-                    b_addr <= b_addr - AXI_DATA_WIDTH/8;
-                    b_rows_count <= b_rows_count + 1;
-
-                    if (b_rows_count == SIZE-1)
-                        state <= WAIT_A;
+                    state <= WAIT_A;
                 end
             end
                 
             WAIT_A: begin
                 if (arvalid & arready & awvalid & awready) begin
-                    b_rows_count <= 0;
-                    state <= WAIT_B;
+                    state <= SAVE_C;
                 end
+            end
+                
+            SAVE_C: begin
+                state <= WAIT_B;
             end
 
             default: state <= state;
